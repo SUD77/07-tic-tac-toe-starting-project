79602783867e8f792b317ff9173d1c1ca6b16469
SUD77 <jaiswalsudhanshu20@gmail.com>
2024-03-17 21:23:25 +0530

    check for build

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/07-tic-tac-toe-starting-project/",
   build:{
     outDir: "./docs"
   },
